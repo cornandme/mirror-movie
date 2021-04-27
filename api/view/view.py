f4b99c47cdea6890137cfc5bf9d685ef9f59af55
cornandme <wonsangland@gmail.com>
2021-04-27 21:07:42 +0900

    [api] Add view

--- api/view/view.py
+++ api/view/view.py
@@ -1,18 +1,33 @@
-
-
-def create_endpoints(app, rec_service):
+def create_endpoints(app, rec_service, search_service):
     @app.route('/ping', methods=['GET'])
     def ping():
         return 'pong!'
 
     @app.route('/', methods=['GET'])
     def home():
-        return 'home page'
-
-    @app.route('/movie/<movie_id>', methods=['GET'])
+        newest_rec = rec_service.get_newest_rec()
+        cluster_rec = rec_service.get_cluster_rec()
+        genre_rec = rec_service.get_genre_rec()
+        return {
+            'newest_rec': newest_rec,
+            'cluster_rec': cluster_rec,
+            'genre_rec': genre_rec
+        }
+    
+    @app.route('/movie/<string:movie_id>', methods=['GET'])
     def movie(movie_id):
-        return f'movie_id {movie_id}\'s detail page.'
+        actor_rec = rec_service.get_actor_rec(movie_id)
+        director_rec = rec_service.get_director_rec(movie_id)
+        similar_rec = rec_service.get_similar_rec(movie_id)
+        return {
+            'actor_rec': actor_rec,
+            'director_rec': director_rec,
+            'similar_rec': similar_rec
+        }
 
-    @app.route('/search/<keyword>')
+    @app.route('/search/<path:keyword>')
     def search(keyword):
-        return f'searching {keyword}'
+        search_result = search_service.search_movie(keyword)
+        return {
+            'search_result': search_result
+        }
