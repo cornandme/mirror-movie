e6302b29e12cd3387f7256b37ba5d6953b331b37
cornandme <wonsangland@gmail.com>
2021-05-02 07:29:30 +0900

    [front] Modify movie detail page - rec section names, rec section order, movie title tag level

--- front/src/components/movie_info/movie_info.jsx
+++ front/src/components/movie_info/movie_info.jsx
@@ -41,7 +41,7 @@ class MovieInfo extends Component {
               src={`${process.env.REACT_APP_STILLCUT_SOURCE}${this.props.movieData.movie_info[0].movie_id}.jpg`}
               alt={`${this.props.movieData.movie_info[0].movie_id}`}
             />
-            <h2 className={styles.movieTitle}>{this.props.movieData.movie_info[0].title_kor}</h2>
+            <h3 className={styles.movieTitle}>{this.props.movieData.movie_info[0].title_kor}</h3>
           </div>
           <div className={styles.infoSection}>
             <span className={styles.info}>
@@ -67,9 +67,19 @@ class MovieInfo extends Component {
             </span>
           </div>
           <div className={styles.recSection}>
+            {this.props.movieData.similar_rec && this.props.movieData.similar_rec.length > 0 &&
+                <div className={styles.recContainer}>
+                  <h4 className={styles.recTitle}>유사한 영화</h4>
+                  <MoviesInDetail 
+                    key='similar_rec'
+                    movies={this.props.movieData.similar_rec}
+                    getMovie={this.props.getMovie}
+                  />
+                </div>
+            }
             {this.props.movieData.actor_rec && this.props.movieData.actor_rec.length > 0 &&
               <div className={styles.recContainer}>
-                <h4 className={styles.recTitle}>배우가 출연한 다른 영화</h4>
+                <h4 className={styles.recTitle}>배우가 출연한 영화</h4>
                 <MoviesInDetail 
                   key='actor_rec'
                   movies={this.props.movieData.actor_rec}
@@ -79,20 +89,10 @@ class MovieInfo extends Component {
             }
             {this.props.movieData.director_rec && this.props.movieData.director_rec.length > 0 &&
               <div className={styles.recContainer}>
-                <h4 className={styles.recTitle}>감독, 작가의 다른 영화</h4>
+                <h4 className={styles.recTitle}>스토리, 연출이 마음에 들었다면</h4>
                 <MoviesInDetail 
                   key='director_rec'
                   movies={this.props.movieData.director_rec}
-                  getMovie={this.props.getMovie}
-                />
-              </div>
-            }
-            {this.props.movieData.similar_rec && this.props.movieData.similar_rec.length > 0 &&
-              <div className={styles.recContainer}>
-                <h4 className={styles.recTitle}>이 영화와 유사한 다른 영화</h4>
-                <MoviesInDetail 
-                  key='similar_rec'
-                  movies={this.props.movieData.similar_rec}
                   getMovie={this.props.getMovie}
                 />
               </div>
