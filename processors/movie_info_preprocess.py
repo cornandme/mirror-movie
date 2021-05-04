db2ae38582016fb9c3e723b07563fee6da6db1c0
cornandme <wonsangland@gmail.com>
2021-05-04 16:01:50 +0900

    [process] Add logic - get year column

--- processors/movie_info_preprocess.py
+++ processors/movie_info_preprocess.py
@@ -28,6 +28,24 @@ def main():
         merged2 = pd.merge(merged1, writers, left_on='_id', right_on='movie_id', how='left', validate='one_to_one')
         merged2 = merged2.rename(columns={'_id': 'movie_id', 'name_x': 'director', 'name_y': 'writer'})
         return merged2
+
+    # process 2
+    def get_year_column(df):
+        def year_process(row):
+            date = row['release_date']
+            if len(date) == 0:
+                result = row['title_eng'].split(',')[-1].replace(' ', '')
+                return result if result.isdigit() else ''
+            return date[:4]
+
+        df.loc[:, 'release_year'] = df.apply(year_process, axis=1)
+        return df
+
+    # execute
+    result = merge_staff_columns(movies_df, makers_df)
+    result = get_year_column(result)
+
+    # unload
     s3 = boto3.client(
         's3',
         aws_access_key_id=config['AWS']['AWS_ACCESS_KEY'],
