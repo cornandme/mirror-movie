d7f1ca7bd2ae7622ecaaa137b02a9f7b0d357f64
cornandme <wonsangland@gmail.com>
2021-05-06 23:58:43 +0900

    [front] Add functionality - front rec scroll

--- front/src/components/movies/movies.jsx
+++ front/src/components/movies/movies.jsx
@@ -5,29 +5,103 @@ import Poster from '../poster/poster';
 
 
 class Movies extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      leftScrollButtonDisplay: false,
+      rightScrollButtonDisplay: false,
+      pointer: 0,
+      pointerMax: this.props.movies.length - this.props.posterCount
+    }
+  }
+  
+  onMovieListHover = () => {
+    this.determineLeftScrollButtonDisplay();
+    this.determineRightScrollButtonDisplay();
+  }
+
+  determineLeftScrollButtonDisplay = () => {
+    if (this.state.pointer === 0) {
+      this.setState({ leftScrollButtonDisplay: false });
+      return;
+    }
+    this.setState({ leftScrollButtonDisplay: true });
+  }
+
+  determineRightScrollButtonDisplay = () => {
+    if (this.state.pointer >= this.state.pointerMax) {
+      this.setState({ rightScrollButtonDisplay: false });
+      return;
+    }
+    this.setState({ rightScrollButtonDisplay: true });
+  }
+
+  onMovieListLeave = () => {
+    this.setState({
+      leftScrollButtonDisplay: false,
+      rightScrollButtonDisplay: false
+    });
+  }
+
+  onLeftScrollButtonClick = () => {
+    const pointer = Math.max(0, this.state.pointer - this.props.posterCount);
+    this.setState({ pointer }, this.onMovieListHover);
+  }
+
+  onRightScrollButtonClick = () => {
+    const pointer = Math.min(this.state.pointerMax, this.state.pointer + this.props.posterCount);
+    this.setState({ pointer }, this.onMovieListHover);
+  }
+
+  componentDidUpdate() {
+    const pointerMax = this.props.movies.length - this.props.posterCount;
+    this.setState({ pointerMax });
+  }
+
   render() {
     return (
       <section className={styles.topicArea}>
         <h4 className={styles.topicTitle}>
           {this.props.id && this.props.id}
         </h4>
-        <ul className={styles.movieList}>
-          {this.props.movies && this.props.movies.slice(0, this.props.posterCount).map((movie) => {
-            const src = `${process.env.REACT_APP_POSTER_SOURCE}${movie.movie_id}.jpg`;
-            return (
-              <li 
-                className={styles.posterContainer} 
-                style={{ width: this.props.posterWidth, height: this.props.posterHeight }}
-              >
-                <Poster 
-                  key={movie.movie_id}
-                  id={movie.movie_id}
-                  src={src}
-                  getMovie={this.props.getMovie}
-                />
-              </li>
-            );
+        <ul 
+          className={styles.movieList}
+          onMouseOver={this.onMovieListHover}
+          onMouseLeave={this.onMovieListLeave}
+        >
+          {this.props.movies && 
+            this.props.movies.slice(this.state.pointer, this.state.pointer + this.props.posterCount).map((movie) => {
+              const src = `${process.env.REACT_APP_POSTER_SOURCE}${movie.movie_id}.jpg`;
+              return (
+                <div 
+                  className={styles.posterContainer} 
+                  style={{ width: this.props.posterWidth, height: this.props.posterHeight }}
+                >
+                  <Poster 
+                    key={movie.movie_id}
+                    id={movie.movie_id}
+                    src={src}
+                    getMovie={this.props.getMovie}
+                  />
+                </div>
+              );
           })}
+          {this.state.leftScrollButtonDisplay &&
+            <div 
+              className={`${styles.scrollButtons} ${styles.leftScrollButton}`}
+              onClick={this.onLeftScrollButtonClick}
+            >
+              <i class="fas fa-chevron-left"></i>
+            </div>
+          }
+          {this.state.rightScrollButtonDisplay &&
+            <div 
+              className={`${styles.scrollButtons} ${styles.rightScrollButton}`}
+              onClick={this.onRightScrollButtonClick}
+            >
+              <i class="fas fa-chevron-right"></i>
+            </div>
+          }
         </ul>
       </section>
     );
