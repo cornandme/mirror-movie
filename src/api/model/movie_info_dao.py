3f9d45cef2bb096c89bb38077aa70bef93adc495
cornandme <wonsangland@gmail.com>
2021-06-05 21:03:58 +0900

    rm useless columns

--- src/api/model/movie_info_dao.py
+++ src/api/model/movie_info_dao.py
@@ -16,4 +16,5 @@ class MovieInfoDAO(S3Loader):
     def load_movie_info(self):
         self.movie_info = self.load_from_s3(self.config['AWS']['S3_BUCKET'], self.config['DATA']['MOVIE_INFO'])
         self.movie_info = self.movie_info.set_index('movie_id').sort_index()
+        self.movie_info.drop(columns=['poster_url', 'stillcut_url'], inplace=True)
         
